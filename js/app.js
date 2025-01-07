7a5f7c54f5e202dba9f8ab68aa5635ca38028949
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-07 11:29:08 -0300

    Aplicando confirmação de devolução

--- js/app.js
+++ js/app.js
@@ -6,10 +6,17 @@ function alterarStatus(index){
    let btn = game.querySelector('.dashboard__item__button');
    let img = game.querySelector('.dashboard__item__img');
 
+   
    if(img.classList.contains('dashboard__item__img--rented')){
-    btn.textContent = 'Alugar';
-    btn.classList.remove('dashboard__item__button--return');
-    img.classList.remove('dashboard__item__img--rented');
+        let pg = prompt("Confirma a devolução? (S/N)")
+
+         if( pg.toUpperCase() == 'S'){
+            btn.textContent = 'Alugar';
+            btn.classList.remove('dashboard__item__button--return');
+            img.classList.remove('dashboard__item__img--rented');
+         }
+       
+
    } else {
     btn.textContent = 'Devolver';
     btn.classList.add('dashboard__item__button--return');
