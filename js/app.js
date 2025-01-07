3cb590b9251cc680156d0c3656882b51f43f401d
FabianoLuiz3103 <fabianojesus1991@gmail.com>
2025-01-07 11:12:23 -0300

    refatorando if/else

--- js/app.js
+++ js/app.js
@@ -6,14 +6,15 @@ function alterarStatus(index){
    let btn = game.querySelector('.dashboard__item__button');
    let img = game.querySelector('.dashboard__item__img');
 
-   if(btn.innerHTML == 'Alugar'){
-        btn.innerHTML = 'Devolver';
-        btn.classList.add('dashboard__item__button--return');
-        img.classList.add('dashboard__item__img--rented');
-   } else {
+   if(img.classList.contains('dashboard__item__img--rented')){
     btn.innerHTML = 'Alugar';
     btn.classList.remove('dashboard__item__button--return');
     img.classList.remove('dashboard__item__img--rented');
+   } else {
+    btn.innerHTML = 'Devolver';
+    btn.classList.add('dashboard__item__button--return');
+    img.classList.add('dashboard__item__img--rented');
+   
 
    }
    
